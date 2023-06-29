35ddbca5bac90ccc65e428c916d1c4a061bac437
nancy <smithnancy@outlook.com>
2023-06-29 20:30:00 +1000

    app global styles added

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,50 @@
 </script>
 
 <template>
-  
+  <div>
+    <header>
+      <h1>Admin Expenses</h1>
+    </header>
+    <Filter/>
+  </div>
 </template>
 
-<style scoped>
-
+<style>
+  :root{
+    --blue: #3b82f6;
+    --white: #fff;
+    --light-gray: #f5f5f5;
+    --gray: #94a3b8;
+    --dark-gray: #64748b;
+    --black: #000;
+  }
+  html{
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *:after{
+    box-sizing: inherit;
+  }
+  body {
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--light-gray);
+  }
+  h1{
+    font-size: 4rem;
+  }
+  h2{
+    font-size: 3rem;
+  }
+  header{
+    background-color: var(--blue);
+  }
+  header h1{
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--white);
+    text-align: center;
+  }
 </style>

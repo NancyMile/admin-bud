4cd849f171175bd172fe8035e2851fa1b782ca23
nancy <smithnancy@outlook.com>
2023-06-29 21:08:26 +1000

    budget component  form styles defined

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-    <form>
+    <form class="budget">
         <div class="campo">
             <label for="new-budget">Budget</label>
             <input
-                type="text"
+                type="number"
                 id="new-budget"
                 class="new-budget"
                 placeholder="Add budget"
@@ -18,4 +18,40 @@
 </template>
 
 <style scoped>
+    .budget {
+        width: 100%;
+    }
+    .campo {
+        display: grid;
+        gap: 2rem;
+    }
+    .budget label{
+        font-size: 2.2rem;
+        text-align: center;
+        color: var(--blue);
+    }
+    .budget input[type="number"]{
+        background-color: var(--light-gray);
+        border-radius: 1rem;
+        padding: 1rem;
+        border: none;
+        font-size: 2.2rem;
+        text-align:center;
+    }
+    .budget input[type="submit"]{
+        background-color: var(--blue);
+        border:none;
+        padding: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 2rem;
+        color: var(--white);
+        font-weight: 900;
+        width: 100%;
+        transition: background-color 300ms ease;
+    }
+    .budget input[type="submit"]:hover{
+        background-color: #1048a4;
+        cursor: pointer;
+    }
 </style>

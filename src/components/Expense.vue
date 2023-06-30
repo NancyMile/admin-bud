43d1431c4c933e4124a1491c1900b322d84652f6
nancy <smithnancy@outlook.com>
2023-06-30 17:33:35 +1000

    modal expense styles applied

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -28,23 +28,69 @@
 
 <template>
     <div class=" expense shadow">
-        <div class="contenido">
+        <div class="container">
             <img
                 :src="iconsDiccionary[expense.category]"
                 alt="Icon"
-                class="Icon"
+                class="icon"
             />
-
             <div class="details">
                 <p class="category">{{ expense.category }}</p>
+                <p class="name">{{ expense.name}}</p>
+                <p class="date">
+                    Date:
+                    <span>{{ dateFormat(expense.date) }}</span>
+                </p>
             </div>
         </div>
         <p class="amount">{{ formatAmount(expense.amount) }}</p>
-        <p class="name">{{ expense.name}}</p>
-
-        <p class="date">{{ dateFormat(expense.date) }}</p>
     </div>
 </template>
 
 <style scoped>
+    .expense{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 2rem;
+
+    }
+    .container{
+        display: flex;
+        gap: 2rem;
+
+    }
+    .icon{
+        width: 5rem;
+    }
+    .details p{
+        margin: 0 0 1rem 0;
+    }
+    .category {
+        color: var(--gray);
+        font-size: 1.2rem;
+        text-transform: uppercase;
+        font-weight: 900;
+    }
+    .name {
+        color: var(--dark-gray);
+        font-size: 2,4rem;
+        font-weight: 700;
+        cursor: pointer;
+
+    }
+    .date {
+        color: var(--dark-gray);
+        font-size: 1.6rem;
+        font-weight: 900;
+    }
+    .date span {
+        font-weight: 400;
+    }
+    .amount{
+        font-size: 3rem;
+        font-weight: 900;
+        margin: 0;
+    }
+
 </style>

c1fffdc2e3dcb7718eb62a2f0b76727e3749ae6b
nancy <smithnancy@outlook.com>
2023-06-30 17:12:11 +1000

    expense component  display icon based on category

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,5 +1,22 @@
 <script setup>
     import { formatAmount, dateFormat } from '../helpers'
+    import IconSavings from '../assets/img/icono_ahorro.svg'
+    import IconHome from '../assets/img/icono_casa.svg'
+    import IconFood from '../assets/img/icono_comida.svg'
+    import IconMiscellaneus from '../assets/img/icono_gastos.svg'
+    import IconLeisure from '../assets/img/icono_ocio.svg'
+    import IconHealth from '../assets/img/icono_salud.svg'
+    import IconSuscriptions from '../assets/img/icono_suscripciones.svg'
+
+    const iconsDiccionary = {
+        savings : IconSavings,
+        food : IconFood,
+        home : IconHome,
+        miscellaneus : IconMiscellaneus,
+        leisure : IconLeisure,
+        health : IconHealth,
+        suscriptions : IconSuscriptions
+    }
 
     const props = defineProps({
         expense: {
@@ -12,6 +29,12 @@
 <template>
     <div class=" expense shadow">
         <div class="contenido">
+            <img
+                :src="iconsDiccionary[expense.category]"
+                alt="Icon"
+                class="Icon"
+            />
+
             <div class="details">
                 <p class="category">{{ expense.category }}</p>
             </div>

0b9b8ccd25418592e016cbcc377c5a1ef364f062
nancy <smithnancy@outlook.com>
2023-06-30 16:40:06 +1000

    helpers format date created and  implemented on expense component

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,9 +1,25 @@
 <script setup>
+    import { formatAmount, dateFormat } from '../helpers'
+
+    const props = defineProps({
+        expense: {
+            type: Object,
+            required: true
+        }
+    })
 </script>
 
 <template>
-    <div>
-        <p>Expense</p>
+    <div class=" expense shadow">
+        <div class="contenido">
+            <div class="details">
+                <p class="category">{{ expense.category }}</p>
+            </div>
+        </div>
+        <p class="amount">{{ formatAmount(expense.amount) }}</p>
+        <p class="name">{{ expense.name}}</p>
+
+        <p class="date">{{ dateFormat(expense.date) }}</p>
     </div>
 </template>
 
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -10,3 +10,14 @@ export const generateId = () => {
     const random = Math.random().toString(32).substring(2); //substring(2) remove 2
     return random + date
 }
+
+export const dateFormat = (date) => {
+    const newDate = new Date(date)
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    }
+
+    return newDate.toLocaleDateString('en-EN', options)
+}
